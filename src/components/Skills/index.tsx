19a79dcd22ec6e7171dad05cb80a938d70f6536e
Jonathan <jtellkampf@gmail.com>
2022-05-30 10:30:33 +0100

    Completed styling skills component

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -12,49 +12,67 @@ const Skills: React.FC = () => {
           <h3>Frontend Development</h3>
           <div className="skills__content">
             <article className="skills__details">
-              <BsPatchCheckFill />
-              <h4>HTML</h4>
-              <small>Experienced</small>
+              <BsPatchCheckFill className="skills__details-icon" />
+              <div>
+                <h4>HTML</h4>
+                <small>Experienced</small>
+              </div>
             </article>
             <article className="skills__details">
-              <BsPatchCheckFill />
-              <h4>CSS</h4>
-              <small>Experienced</small>
+              <BsPatchCheckFill className="skills__details-icon" />
+              <div>
+                <h4>CSS</h4>
+                <small>Experienced</small>
+              </div>
             </article>
             <article className="skills__details">
-              <BsPatchCheckFill />
-              <h4>Javascript</h4>
-              <small>Experienced</small>
+              <BsPatchCheckFill className="skills__details-icon" />
+              <div>
+                <h4>Javascript</h4>
+                <small>Experienced</small>
+              </div>
             </article>
             <article className="skills__details">
-              <BsPatchCheckFill />
-              <h4>Typescript</h4>
-              <small>Experienced</small>
+              <BsPatchCheckFill className="skills__details-icon" />
+              <div>
+                <h4>Typescript</h4>
+                <small>Experienced</small>
+              </div>
             </article>
             <article className="skills__details">
-              <BsPatchCheckFill />
-              <h4>SASS</h4>
-              <small>Experienced</small>
+              <BsPatchCheckFill className="skills__details-icon" />
+              <div>
+                <h4>SASS</h4>
+                <small>Experienced</small>
+              </div>
             </article>
             <article className="skills__details">
-              <BsPatchCheckFill />
-              <h4>React</h4>
-              <small>Experienced</small>
+              <BsPatchCheckFill className="skills__details-icon" />
+              <div>
+                <h4>React</h4>
+                <small>Experienced</small>
+              </div>
             </article>
             <article className="skills__details">
-              <BsPatchCheckFill />
-              <h4>NextJS</h4>
-              <small>Experienced</small>
+              <BsPatchCheckFill className="skills__details-icon" />
+              <div>
+                <h4>NextJS</h4>
+                <small>Experienced</small>
+              </div>
             </article>
             <article className="skills__details">
-              <BsPatchCheckFill />
-              <h4>Redux</h4>
-              <small>Experienced</small>
+              <BsPatchCheckFill className="skills__details-icon" />
+              <div>
+                <h4>Redux</h4>
+                <small>Experienced</small>
+              </div>
             </article>
             <article className="skills__details">
-              <BsPatchCheckFill />
-              <h4>Apollo Client</h4>
-              <small>Experienced</small>
+              <BsPatchCheckFill className="skills__details-icon" />
+              <div>
+                <h4>Apollo Client</h4>
+                <small>Experienced</small>
+              </div>
             </article>
           </div>
         </div>
@@ -63,59 +81,81 @@ const Skills: React.FC = () => {
           <h3>Backend Development</h3>
           <div className="skills__content">
             <article className="skills__details">
-              <BsPatchCheckFill />
-              <h4>NodeJS</h4>
-              <small>Experienced</small>
+              <BsPatchCheckFill className="skills__details-icon" />
+              <div>
+                <h4>NodeJS</h4>
+                <small>Experienced</small>
+              </div>
             </article>
             <article className="skills__details">
-              <BsPatchCheckFill />
-              <h4>ExpressJS</h4>
-              <small>Experienced</small>
+              <BsPatchCheckFill className="skills__details-icon" />
+              <div>
+                <h4>ExpressJS</h4>
+                <small>Experienced</small>
+              </div>
             </article>
             <article className="skills__details">
-              <BsPatchCheckFill />
-              <h4>GraphQL</h4>
-              <small>Experienced</small>
+              <BsPatchCheckFill className="skills__details-icon" />
+              <div>
+                <h4>GraphQL</h4>
+                <small>Experienced</small>
+              </div>
             </article>
             <article className="skills__details">
-              <BsPatchCheckFill />
-              <h4>Apollo Server</h4>
-              <small>Experienced</small>
+              <BsPatchCheckFill className="skills__details-icon" />
+              <div>
+                <h4>Apollo Server</h4>
+                <small>Experienced</small>
+              </div>
             </article>
             <article className="skills__details">
-              <BsPatchCheckFill />
-              <h4>MongoDB</h4>
-              <small>Experienced</small>
+              <BsPatchCheckFill className="skills__details-icon" />
+              <div>
+                <h4>MongoDB</h4>
+                <small>Experienced</small>
+              </div>
             </article>
             <article className="skills__details">
-              <BsPatchCheckFill />
-              <h4>Mongoose</h4>
-              <small>Experienced</small>
+              <BsPatchCheckFill className="skills__details-icon" />
+              <div>
+                <h4>Mongoose</h4>
+                <small>Experienced</small>
+              </div>
             </article>
             <article className="skills__details">
-              <BsPatchCheckFill />
-              <h4>SQL</h4>
-              <small>Experienced</small>
+              <BsPatchCheckFill className="skills__details-icon" />
+              <div>
+                <h4>SQL</h4>
+                <small>Experienced</small>
+              </div>
             </article>
             <article className="skills__details">
-              <BsPatchCheckFill />
-              <h4>PostgreSQL</h4>
-              <small>Experienced</small>
+              <BsPatchCheckFill className="skills__details-icon" />
+              <div>
+                <h4>PostgreSQL</h4>
+                <small>Experienced</small>
+              </div>
             </article>
             <article className="skills__details">
-              <BsPatchCheckFill />
-              <h4>Typeorm</h4>
-              <small>Experienced</small>
+              <BsPatchCheckFill className="skills__details-icon" />
+              <div>
+                <h4>Typeorm</h4>
+                <small>Experienced</small>
+              </div>
             </article>
             <article className="skills__details">
-              <BsPatchCheckFill />
-              <h4>Redis</h4>
-              <small>Experienced</small>
+              <BsPatchCheckFill className="skills__details-icon" />
+              <div>
+                <h4>Redis</h4>
+                <small>Experienced</small>
+              </div>
             </article>
             <article className="skills__details">
-              <BsPatchCheckFill />
-              <h4>AWS S3</h4>
-              <small>Experienced</small>
+              <BsPatchCheckFill className="skills__details-icon" />
+              <div>
+                <h4>AWS S3</h4>
+                <small>Experienced</small>
+              </div>
             </article>
           </div>
         </div>
